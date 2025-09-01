d7afa8a815ac545fa358fa36a5eff4b4719458f6
colaksewal <108463004+colaksewal@users.noreply.github.com>
2025-09-01 11:56:18 +0300

    update README.md

--- core/db.php
+++ core/db.php
@@ -1,23 +1,15 @@
 <?php
-$host = 'db.fr-pari1.bengt.wasmernet.com';
-$port = 10272;
-$dbname = 'smarthome';
-$user = '4b1493d17ed380008f49b5efeb4e';
-$pass = '068b4b14-93d2-70d6-8000-99f755139ae8'; // Buraya şifreni yaz
-
+$host = 'localhost';
+$dbname = 'smartlife';
+$user = 'root';
+$pass = '';
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ];
 
 try {
-    $db = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $pass,
-        $options
-    );
-    echo "DB bağlantısı başarılı!";
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, $options);
 } catch (PDOException $e) {
-    die("DB bağlantısı başarısız: " . $e->getMessage());
+    die("DB bağlantısı başarısız: ".$e->getMessage());
 }
